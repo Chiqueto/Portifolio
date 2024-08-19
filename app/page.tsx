f13b6110ec18864ce93f0dd691eeb17896261799
chiqueto <lfchiqueto@gmail.com>
2024-08-19 10:44:23 -0300

    chore: add git hooks and msg-linter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="mb-5 bg-red-600 text-red-50">
-      <p className="mb-5 py-2 text-zinc-100"></p>
+    <main className="">
+      {" "}
+      <p>teste</p>
     </main>
   )
 }
